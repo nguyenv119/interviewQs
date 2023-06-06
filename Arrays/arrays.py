5517ab8e64df71745b906e8a5ac3b61c93f5472c
Long Nguyen <nguyenv@brandeis.edu>
2023-06-05 21:47:09 -0700

    MatrixSum

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -2,17 +2,41 @@ from typing import List
 class Solution:
 
     def test(self):
-        nums = [1,1,1,2,2,3]
-        assert self.topKFrequent(nums, 2) == [1, 2]
+        matrix = [[7,2,1],[6,4,2],[6,5,3],[3,2,1]]
+        assert self.matrixSum(matrix) == 15
 
-        nums = [1]
-        assert self.topKFrequent(nums, 1) == [1]
+        matrix = [[1]]
+        assert self.matrixSum(matrix) == 1
 
-        nums = [4,1,-1,2,-1,2,3]
-        assert self.topKFrequent(nums, 2) == [-1, 2]
+        matrix = [[1,8,16,15,12,9,15,11,18,6,16,4,9,4],[3,19,8,17,19,4,9,3,2,10,15,17,3,11],[13,10,19,20,6,17,15,14,16,8,1,17,0,2],[12,20,0,19,15,10,7,10,2,6,18,7,7,4],[17,14,2,2,10,16,15,3,9,17,9,3,17,10],[17,6,19,17,18,9,14,2,19,12,10,18,7,9],[5,6,5,1,19,8,15,2,2,4,4,1,2,17],[12,16,8,16,7,6,18,13,18,8,14,15,20,11],[2,10,19,3,15,18,20,10,6,7,0,8,3,7],[11,5,10,13,1,3,4,7,1,18,20,17,19,2],[0,3,20,6,19,18,3,12,2,11,3,1,19,0],[6,5,3,15,6,1,0,17,13,19,3,8,2,7],[2,20,9,11,13,5,1,16,14,1,19,3,12,6]]
+        assert self.matrixSum(matrix) == 190
 
         print("All Tests Passed Succesfully!")
     
+
+    def matrixSum(self, nums: List[List[int]]) -> int:
+        if nums == [[]]:
+            return 0
+
+        score = 0
+        return self.helperMatrixSum(nums, score)
+    
+    def helperMatrixSum(self, nums: List[List[int]], score: int) -> int:
+        if nums == [[]]:
+            return 0
+        
+        globalMax = float('-inf')
+        for i in range(len(nums)):
+            localMax = max(nums[i])
+            globalMax = max(globalMax, localMax)
+            nums[i].remove(localMax)
+
+        score += int(globalMax)
+
+        if len(nums[0]) >= 1: score = self.helperMatrixSum(nums, score)
+        return score
+    
+
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         if k < 0 or nums == []: return []
         if len(nums) == 1: return nums
