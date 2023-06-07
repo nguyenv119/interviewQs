5b7ffbe4802543634af187b345ffde2f999ad866
Long Nguyen <nguyenv@brandeis.edu>
2023-06-07 01:15:00 -0700

    minStack

--- Stacks/stack.py
+++ Stacks/stack.py
@@ -1,55 +1,45 @@
-from typing import List
 class MinStack:
-    arr: List[int]
-    currMin: int
-    '''
-    currMin = inf
-    currMin2 = inf
-    [1, -10]
-
-    if length is 2: if 
-    '''
 
     def __init__(self):
-        arr = list()
+        self.stack = list()
+        self.stackMin = list()
 
     def push(self, val: int) -> None:
-        self.arr.append(val)
-
-        if len(self.arr) >= 2:
-            if val <= self.currMin: 
-                self.currMin2 = self.currMin
-                self.currMin = val
-            else: self.currMin2 = val
-
-        else: self.currMin = val
-
+        self.stack.append(val)
+        if self.stackMin: self.stackMin.append(min(val, self.stackMin[-1]))
+        else: self.stackMin.append(val)
         return None
 
     def pop(self) -> None:
-        self.arr.remove(self.arr[0])
-        self.currMin = self.currMin2
-
+        if self.stack: self.stack.pop()
+        if self.stackMin: self.stackMin.pop()
         return None
 
     def top(self) -> int:
-        return self.arr[0]
+        return self.stack[len(self.stack) - 1]
 
     def getMin(self) -> int:
-        return self.currMin
+        return self.stackMin[len(self.stack) - 1]
 
     def test(self):
         minStack =  MinStack()
-        assert print(minStack) == []
+        assert minStack.stack == []
         minStack.push(-2)
-        assert print(minStack) == [-2]
+        assert minStack.stack == [-2]
         minStack.push(0)
-        assert print(minStack) == [-2, 0]
+        assert minStack.stack == [-2, 0]
         minStack.push(-3)
-        assert print(minStack) == [-2, 0, -3]
+        assert minStack.stack == [-2, 0, -3]
         assert minStack.getMin() == -3
+        minStack.push(-3)
+        assert minStack.stack == [-2, 0, -3, -3]
+        assert minStack.getMin() == -3
+        minStack.pop()
         minStack.pop()
         assert minStack.top() == 0
         assert minStack.getMin() == -2
 
         print("All Tests Passed Succesfully!")
+
+minStack =  MinStack()
+minStack.test()
