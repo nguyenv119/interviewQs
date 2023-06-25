8debc7cd8268f954e9059b6c3330a79f7997fb53
Long Nguyen <nguyenv@brandeis.edu>
2023-06-24 23:26:11 -0700

    generateParenthesis

--- Stacks/stack.py
+++ Stacks/stack.py
@@ -21,6 +21,54 @@ class MinStack:
     def getMin(self) -> int:
         return self.stackMin[len(self.stack) - 1]
 
+    def generateParenthesis(self, n: int) -> List[str]:
+        '''
+        There is a max of n ( and n ) in each parentheses
+        We need a way to determine what is a valid parentheses?
+
+        --> When there is a starting ( that eventually ends with )
+        How many ways can we rearrange ( such that it eventually ends with )
+
+        For every x number of "(" we have, there are at most x number of ")":
+        - ()) cannot work
+
+        - We can keep adding ( n times, and after each time we add, we can either
+        add another (, or a ), this is until we have completely added n (s, so in
+        that case we have to add the remaining )
+        --> should have a var calculating how many ( and ) we have added
+
+        - beginning( and end) variables start out the same
+        - When they are the same, we must add (, and when beginning < end, we can
+        add either
+
+        We can treat it like a stack, decision tree. Every time we add, we have 
+        to see the restrictions on the () variables, and then call it recursively.
+        Every time we finish adding, we pop as to rewind in the tree and allow
+        other combinations to form
+
+        '''
+
+        stack = list()
+        res = list()
+
+        def helper(openAdded: int, closeAdded: int):
+            if openAdded == closeAdded == n:
+                res.append("".join(stack))
+                return
+            
+            if openAdded < n:
+                stack.append("(")
+                helper(openAdded + 1, closeAdded)
+                stack.pop()
+            
+            if closeAdded < openAdded:
+                stack.append(")")
+                helper(openAdded, closeAdded + 1)
+                stack.pop()
+
+        helper(0, 0)
+        return res
+    
     def test(self):
         minStack =  MinStack()
         assert minStack.stack == []
