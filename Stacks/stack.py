4aa103ae1e2256575807f7d0abb6ac6d798e7250
Long Nguyen <nguyenv@brandeis.edu>
2023-07-10 20:53:39 -0700

    dailyTemp

--- Stacks/stack.py
+++ Stacks/stack.py
@@ -1,3 +1,6 @@
+from typing import List
+from collections import deque
+
 class MinStack:
 
     def __init__(self):
@@ -69,23 +72,61 @@ class MinStack:
         helper(0, 0)
         return res
     
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        '''
+        We have to return an array, where each i'th element corresponds to the i'th temperature,
+        and whose value is the number of days we have to wait until it gets warmer, or until
+        we see a value larger than it
+
+        [1, 2, 3, 4]
+        [1, 1, 1, 0]
+
+        Brute force: we can go through every element, and examine every other element in the array,
+        keeping track of the spaces between the original element and of where the nearest value
+        larger than it is: O(n^2) time, since we have to iterate through the array n(n-1) / 2 times
+
+        When we go through, we should remember what was before it so we can resolve if necesarry.
+        We know that every number in this previous spot, will be in descending order, so When
+        we find something, we can resolve all of these.
+
+        For instance, [1, 2, 3, 4] If they were not in descending order, we would continuously
+        resolve the numbers 1 at a time. However: [3, 2, 1, 4], 4 would resolve all 3, 2, 1. But
+        what about [3, 2, 1, 2] 2 would only resolve one of them, so how would we resolve and keep 
+        it at the same time for 3.
+
+        We can use a regular list to keep track of the numbers. We know that the result is the same size
+        as the original list. So, in this case, we can resolve it by adding 1 at the 2nd index, but since
+        our checking array is the same size as the original array, we know its index, and can keep track
+        of how far temps are from one another
+
+        1. Make checking list same size as temperature
+        2. Add 1st element and start at the 1th index
+        3. Go through list: if number is bigger than top of stack, add distance between indices: i'th index and index of top of stack
+        4. Pop top of stack
+
+
+
+        Theta(n) in space since we are adding every time every element to our queue
+        Theta(n) in time since we always iterate through the array once, and pop n elements
+
+        [73,74,75,71,69,72,76,73]
+        res = [1,1,0,2,1,0,0,0]
+        check = [73_, 74_, 75, 71_, 69_, 72] 
+        '''
+        res = [0] * len(temperatures)
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
+                stackTemp, stackIdx = stack.pop()
+                res[stackIdx] = i - stackIdx
+            stack.append([temp, i])
+        return res
+    
     def test(self):
-        minStack =  MinStack()
-        assert minStack.stack == []
-        minStack.push(-2)
-        assert minStack.stack == [-2]
-        minStack.push(0)
-        assert minStack.stack == [-2, 0]
-        minStack.push(-3)
-        assert minStack.stack == [-2, 0, -3]
-        assert minStack.getMin() == -3
-        minStack.push(-3)
-        assert minStack.stack == [-2, 0, -3, -3]
-        assert minStack.getMin() == -3
-        minStack.pop()
-        minStack.pop()
-        assert minStack.top() == 0
-        assert minStack.getMin() == -2
+        assert self.dailyTemperatures([73,74,75,71,69,72,76,73]) == [1,1,4,2,1,1,0,0]
+        assert self.dailyTemperatures([30,40,50,60]) == [1,1,1,0]
+        assert self.dailyTemperatures([30,60,90]) == [1,1,0]
 
         print("All Tests Passed Succesfully!")
 
