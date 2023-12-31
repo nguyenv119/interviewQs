b6f1bb14cb6ce2344b0a6005b013e502bf80e629
Long Nguyen <nguyenv@brandeis.edu>
2023-12-30 17:09:20 -0800

    isBalanced java

--- Java/Trees/trees.java
+++ Java/Trees/trees.java
@@ -12,7 +12,31 @@ class trees {
         trees t = new trees();
         System.out.println(t.diameterOfBinaryTree(root));
     }
+
+    /**
+     * isBalanced
+     */
+    boolean balanced = true;
+    public boolean isBalanced(TreeNode root) {
+        /** balanced if abs(lH - rH) <= 1, return this and left and right. But need to get lH and rH before this */
+        if (root == null) return true;
+        getHeight(root); /** In calculating height of root, we have height of all others, no need to call again */
+        return balanced;
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) return 0;
+        int lH = getHeight(root.left);
+        int rH = getHeight(root.right);
+
+        if (Math.abs(lH - rH) > 1) balanced = false;
+
+        return 1 + Math.max(lH, rH);
+    }
     
+    /**
+     * maxDiameter
+     */
     int maxDiam = 0;
     public int diameterOfBinaryTree(TreeNode root) {
         if (root == null) return 0;
